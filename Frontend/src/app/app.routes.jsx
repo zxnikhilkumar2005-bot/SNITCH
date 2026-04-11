b08199a7b907740bb0d65a9dab9e32a530a5662f
Nikhil kumar <zxnihilkumar2005@gmail.com>
2026-04-11 11:03:20 +0530

    feat: add Register component and update routing for registration

--- Frontend/src/app/app.routes.jsx
+++ Frontend/src/app/app.routes.jsx
@@ -1,9 +1,14 @@
 import { createBrowserRouter } from "react-router";
+import Register from "../features/auth/pages/Register";
 
 
 export const router = createBrowserRouter([
     {
         path: "/",
         element: <h1>hello world</h1>
+    },
+    {
+        path: "/register",
+        element: <Register />
     }
 ])
